13cbe37d20200ba27f6ebb37ea523e0a18b13f50
mfouladi <m.fouladi@pouyancorp.ir>
2023-07-01 17:44:38 +0330

    ADD: nginx conf & http api added

--- chat/api.py
+++ chat/api.py
@@ -1,3 +1,4 @@
+from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,11 +9,13 @@ import json
 def send_message(request):
     channel_layer = get_channel_layer()
     message = request.POST.get('message')
-    channel_layer.group_send(
-        'chat_lobby',
-        {
-            'type': 'chat_message',
-            'message': message
-        }
-    )
+    async_to_sync(channel_layer.group_send)(
+            'chat_varzesshi',
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+
     return HttpResponse('Message sent')
